e09bc9416102c8919633b867bfd452d484723e0e
aarondtcs <aarond-farmington@thecoderschool.com>
2024-11-20 23:55:59 +0000

    I added new lists for the hangman. I added the underlines.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import random
-wordList = ["apple", "banana", "tools", "computer", "suspicous", "arizona", "connecticut", "operation","opposition", "kryptonite"]
-gameWord = wordList[random.randint(0, len(wordList)-1)]
-print(gameWord)
+wordList = ["apple", "banana", "computer", "suspicous", "arizona", "connecticut", "operation","opposition", "kryptonite"]
+gameWord = wordList[random.randint(0, len(wordList)-1)] #prints random thing from wordList
+gL = [] # letters from word list
+gL.extend(gameWord)
+uL = [] #uL stands for under lines
+
+for i in range(len(gL)):
+    uL.append("_")
+
+print(uL)
